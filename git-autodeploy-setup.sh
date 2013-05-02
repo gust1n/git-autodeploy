ee2b08df11466c4bc273e1456414a42c38a15a8a
Jocke Gustin <jocke.gustin@gmail.com>
2013-05-02 12:04:43 +0200

    Dont cd into folders, it ugly and unneccesary

--- git-autodeploy-setup.sh
+++ git-autodeploy-setup.sh
@@ -38,52 +38,48 @@ mkdir $DIR
 fi
 
 #Non-bare repo solution
-cd $DIR
+mkdir $DIR/$DIR_CONTENT
+mkdir $DIR/$DIR_REPO
+mkdir $DIR/$DIR_LOGS
 
-mkdir $DIR_CONTENT
-mkdir $DIR_REPO
-mkdir $DIR_LOGS
-
-cd $DIR_CONTENT
-
-#Become the web user that should own the deployment keys
+#Become the web user (that should own the deployment keys)
 su $WEB_USER
 
-git --git-dir=$DIR/$DIR_REPO --work-tree=. init 
-echo "gitdir: $DIR/$DIR_REPO" > .git
-chmod og-rx .git #(possibly) secure the file
+git --git-dir=$DIR/$DIR_REPO --work-tree=. init $DIR/$DIR_CONTENT
+echo "gitdir: $DIR/$DIR_REPO" > $DIR/$DIR_CONTENT/.git
+chmod og-rx $DIR/$DIR_CONTENT/.git #(possibly) secure the file
+
+cd $DIR/$DIR_CONTENT
 git remote add -t $BRANCH -f origin $REMOTE_REPO
 git checkout $BRANCH
 
 #Create simple deploy.php file to hook to bitbucket/github sericehooks
-cat > deploy.php << EOF
+cat > $DIR/$DIR_CONTENT/deploy.php << EOF
 <?php exec('git pull'); ?>
 EOF
 
 #Add deploy file to .gitignore
 #echo "deploy.php"  >>  .gitignore
 
-cd $DIR/$DIR_REPO
-
 #This is an ugly but cool and working method,
 #To allow for pushing directly to this repo we have to check out another
 #branch pre-receive and then switch back post-receive
 
 #Switch to temp branch to be able to push to this repos selected branch
-cat > hooks/pre-receive << EOF
+cat > $DIR/$DIR_REPO/hooks/pre-receive << EOF
 #!/bin/sh
 git checkout -b tmp
 EOF
 
 #Switch back and delete branch
-cat > hooks/post-receive << EOF
+cat > $DIR/$DIR_REPO/hooks/post-receive << EOF
 #!/bin/sh
 git checkout $BRANCH; git branch -d tmp
 EOF
 
 #Make hooks excecutable
-chmod +x hooks/pre-receive
-chmod +x hooks/post-receive
+chmod +x $DIR/$DIR_REPO/hooks/pre-receive
+chmod +x $DIR/$DIR_REPO/hooks/post-receive
 
 #Make www-data user owner of the files
 chown -R $WEB_USER:$WEB_USER_GROUP $DIR
